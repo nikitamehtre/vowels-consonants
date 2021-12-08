806dfc7295a9ab088f73016b0adc4c38fa2256f7
Nikita Mehtre <mehtrenikita5@gmail.com>
2021-12-08 18:21:37 +0530

    Initial commit

---
+++ src/alphabets/vowels.java
@@ -0,0 +1,34 @@
+package alphabets;
+
+import java.util.Scanner;
+
+public class vowels {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter an aplphabet ");
+		Scanner sc = new Scanner(System.in);
+		
+		//char letter = 'i';
+		char letter = sc.next().charAt(0);
+		switch (letter)
+        {
+            case 'a':	
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println(letter + " Letter is vowel");
+            break;
+       
+            default:
+                System.out.println(letter + " Letter is consonant");
+            break;
+		}
+		    
+
+	}
+
+}
+			
+		
