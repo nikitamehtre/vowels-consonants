59ac38d5fa348152787fac4fe0d23d7840df8b2a
Nikita Mehtre <mehtrenikita5@gmail.com>
2021-12-08 18:32:48 +0530

    Added capital letters support

--- src/alphabets/vowels.java
+++ src/alphabets/vowels.java
@@ -12,11 +12,16 @@ public class vowels {
 		char letter = sc.next().charAt(0);
 		switch (letter)
         {
-            case 'a':	
+            case 'a':
+            case 'A':
             case 'e':
+            case 'E':
             case 'i':
+            case 'I':
             case 'o':
+            case 'O':
             case 'u':
+            case 'U':
                 System.out.println(letter + " Letter is vowel");
                 break;
             default:
